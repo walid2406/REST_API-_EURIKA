bfc0c2c958046375324cad5fb80e605562e3ef74
walid.abboud.tech <walid.abboud.tech@gmail.com>
2021-12-24 09:15:46 +0100

    all done with eureka well undrestand mivroservices communication and discovery ready to next step

--- movie-catalog-service/src/main/java/walid/abboud/tech/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/walid/abboud/tech/moviecatalogservice/resource/MovieCatalogResource.java
@@ -52,7 +52,7 @@ public class MovieCatalogResource {
 
 
                 //get all rated movies id
-                UserRating ratings =restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class
+                UserRating ratings =restTemplate.getForObject("http://rating-data-service/ratingsdata/users/"+userId, UserRating.class
                 );
                         //ParameterizedType <List<Rating>>
 
@@ -72,7 +72,7 @@ public class MovieCatalogResource {
 //for each movie id call movie info services and get detail
                 return  ratings.getUserRating().stream().map(rating ->{
                     //this line will be replaced by new builder
-                    Movie movie= restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+                    Movie movie= restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
 
                             //bodytoMono== what ever instance u collect convert it to instance of movie
                             //on this workshop still use restTemplate
@@ -100,3 +100,85 @@ public class MovieCatalogResource {
 
     }
 }
+/* ************************hard coded version without eureka *********************************
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webckientBuilder;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+        //version reactive
+        //create a method as @bean and call it as needed ==> was creted on main class
+        //autowired that method here
+
+
+
+
+
+
+        //create a rest api
+        //ici la creation a ete faite avent bean and autowired so pas besoin de l instancier
+        //RestTemplate restTemplate=new RestTemplate();
+        //call the rest api for each instance of movie a inclure dans boocle
+       //Movie movie= restTemplate.getForObject("http://localhost:8082/movies/zab", Movie.class);
+
+
+
+                //get all rated movies id
+                UserRating ratings =restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class
+                );
+                        //ParameterizedType <List<Rating>>
+
+
+
+                /* ici ancienne version pour genere des list rating
+                Arrays.asList(
+                        //new Rating("1234",4),
+                        //new Rating("5678",3)
+
+                * a fermer comment /
+
+
+
+
+
+//for each movie id call movie info services and get detail
+                return  ratings.getUserRating().stream().map(rating ->{
+                        //this line will be replaced by new builder
+                        Movie movie= restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+
+        //bodytoMono== what ever instance u collect convert it to instance of movie
+        //on this workshop still use restTemplate
+        //haw to create a builder reactive
+                           /* Movie movie=  webckientBuilder.build()
+                                    .get()
+                                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                                    .retrieve()
+                                    .bodyToMono(Movie.class)
+                                    .block();
+
+                           a fermer comment /
+
+        //put them all together
+        return  new CatalogItem(movie.getMovieId(),"Description",rating.getRating());
+        })
+        .collect(Collectors.toList());
+
+
+
+
+//        return Collections.singletonList(
+//                new CatalogItem("transformer","test",4)
+//        );
+
+        }
+        }
+
+ */
--- rating-data-service/src/main/java/walid/abboud/tech/ratingdataservice/RatingDataServiceApplication.java
+++ rating-data-service/src/main/java/walid/abboud/tech/ratingdataservice/RatingDataServiceApplication.java
@@ -2,8 +2,10 @@ package walid.abboud.tech.ratingdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingDataServiceApplication {
 
 	public static void main(String[] args) {
